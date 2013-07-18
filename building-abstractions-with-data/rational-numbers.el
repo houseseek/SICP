b2e0a94d3ca807722fb6e58636dffc68ab81659f
houseseek <houseseek@163.com>
2013-07-18 10:12:15 +0800

    An example from Chapter 2.

---
+++ building-abstractions-with-data/rational-numbers.el
@@ -0,0 +1,45 @@
+(defun add-rat (x y)
+  (make-rat (+ (* (numer x) (denom y))
+	       (* (numer y) (denom x)))
+	    (* (denom x) (denom y))))
+
+(defun sub-rat (x y)
+  (make-rat (- (* (numer x) (denom y))
+	       (* (numer y) (denom x)))
+	    (* (denom x) (denom y))))
+
+(defun mul-rat (x y)
+  (make-rat (* (numer x) (numer y))
+	    (* (denom x) (denom y))))
+
+(defun div-rat (x y)
+  (make-rat (* (numer x) (denom y))
+	    (* (denom x) (numer y))))
+
+(defun equal-rat? (x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(defun make-rat (x y)
+  (defun gcd (a b)
+    (if (= b 0)
+	a
+      (gcd b (% a b))))
+  (let ((c (gcd x y)))
+    (cons (/ x c) (/ y c))))
+(defun numer (x) (car x))
+(defun denom (x) (cdr x))
+
+(defun print-rat (x)
+  (princ (format "%d/%d\n" (numer x) (denom x))))
+
+(defconst one-half (make-rat 1 2))
+
+(print-rat one-half)
+
+(defconst one-third (make-rat 1 3))
+(print-rat (add-rat one-half one-third))
+(print-rat (mul-rat one-half one-third))
+(print-rat (div-rat one-half one-third))
+(print-rat (sub-rat one-half one-third))
+(print-rat (add-rat one-third one-third))
